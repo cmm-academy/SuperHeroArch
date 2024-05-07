249b6b037c264228d9548511e59b48f3da2d0278
Pablo Grela <p.martinez.grela@accenture.com>
2024-05-07 16:22:29 +0200

    Added episode details on character details.

--- app/src/main/java/com/mstudio/superheroarch/ApiResponseWrapper.kt
+++ app/src/main/java/com/mstudio/superheroarch/ApiResponseWrapper.kt
@@ -3,3 +3,9 @@ package com.mstudio.superheroarch
 data class ApiResponseWrapper(
     val results: List<Character>
 )
+
+data class Episode(
+    val name: String,
+    val air_date: String,
+    val episode: String
+)
