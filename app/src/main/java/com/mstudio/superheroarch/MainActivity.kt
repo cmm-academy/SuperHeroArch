93c1ca410635cc434724df5f96b849b52459d0d4
Ramos, Yago <>
2024-06-14 11:35:33 +0200

    Completed step1

--- app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.mstudio.superheroarch
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        
+        val mainButton : Button = findViewById(R.id.main_button)
+        val mainText = findViewById<TextView>(R.id.title)
+
+        mainButton.setOnClickListener{
+            val handler = Handler()
+            val runnable = Runnable {
+                mainText.text = getString(R.string.main_title)
+            }
+            mainText.setText(getString(R.string.main_text_clicked))
+            handler.postDelayed(runnable, 3000)
+        }
+
     }
 }
