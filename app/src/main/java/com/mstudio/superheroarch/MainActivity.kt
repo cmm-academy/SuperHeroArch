961379db87f6fd76a2a960ed33633d207c66af83
Ramos, Yago <>
2024-06-17 17:21:34 +0200

    Step 2

--- app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
@@ -7,6 +7,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +26,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        
-        val mainButton = findViewById<Button>(R.id.main_button)
-        val mainText = findViewById<TextView>(R.id.title)
 
-        mainButton.setOnClickListener{
-            mainText.text = getString(R.string.main_text_clicked)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiRick = retrofit.create(ApiRick::class.java)
+        val titleTextView = findViewById<TextView>(R.id.title)
+        val button = findViewById<Button>(R.id.main_button)
+
+        button.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = apiRick.getCharacter()
+
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    runOnUiThread {
+                        titleTextView.text = responseBody.toString()
+                    }
+                } else {
+                    runOnUiThread {
+                        Snackbar.make(findViewById(R.id.main), "Failed to fetch data", Snackbar.LENGTH_LONG).show()
+                    }
+                }
+            }
         }
     }
+}
 
+interface ApiRick {
+    @GET("character")
+    suspend fun getCharacter(): Response<Any>
 }
