692fcea834590aeb4dff95bc5b321ac6dcfa738b
Alessandra Dosil <alessandra.dosil@accenture.com>
2024-06-19 14:05:07 +0200

    Step 1

--- app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
