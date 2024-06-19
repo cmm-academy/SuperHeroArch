dc63e571bc6375123c6a484aadf15912fe7edf9c
Alessandra Dosil <alessandra.dosil@accenture.com>
2024-06-19 14:03:47 +0200

    Step 1

--- app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.mstudio.superheroarch
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val varButton = findViewById<Button>(R.id.button)
+        val varText = findViewById<TextView>(R.id.title)
+        val varNewText = "Texto cambiado"
+        varButton.setOnClickListener {
+            Log.d("tag", "Botón pulsado")
+            varText.setText(varNewText)
         }
     }
 }
