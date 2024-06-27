74f2caa880e130391b9d8fa4b7e958d173f0fd3e
Alessandra Dosil <alessandra.dosil@accenture.com>
2024-06-27 17:45:41 +0200

    Step 2: Network call

--- app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
@@ -1,19 +1,48 @@
 package com.mstudio.superheroarch
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 class MainActivity : AppCompatActivity() {
+
+    interface RetrofitService {
+        @GET("characters∫")
+        suspend fun getUser(): Response<JsonObject>
+    }
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://rickandmortyapi.com/api/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.primary_button)
         val text = findViewById<TextView>(R.id.title)
+        val service = retrofit.create(RetrofitService::class.java)
 
         button.setOnClickListener {
-            text.text = getString(R.string.new_text)
+            CoroutineScope(Dispatchers.IO).launch {
+                val responseGetUser = service.getUser()
+                withContext(Dispatchers.Main) {
+                    if (responseGetUser.isSuccessful) {
+                        text.text = responseGetUser.body().toString()
+                    } else {
+                        text.text = getString(R.string.error_api)
+                    }
+                }
+            }
         }
     }
 }
