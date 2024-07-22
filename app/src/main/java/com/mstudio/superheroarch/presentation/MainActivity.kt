987294228f4fdd9cc040aa9a5558a214d6c32474
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-07-22 15:09:50 +0200

    pr changes

--- app/src/main/java/com/mstudio/superheroarch/presentation/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/presentation/MainActivity.kt
@@ -2,35 +2,22 @@ package com.mstudio.superheroarch.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mstudio.superheroarch.R
 import com.mstudio.superheroarch.databinding.MainActivityBinding
-import org.koin.android.ext.android.inject
-import org.koin.core.component.KoinComponent
-import org.koin.core.parameter.parametersOf
 
-class MainActivity : AppCompatActivity(), MainActivityViewContract, KoinComponent {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
-    private val presenter: MainActivityPresenter by inject { parametersOf(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        presenter.onCreate()
-        binding.mainButton.setOnClickListener {
-            presenter.onMainButtonClicked()
-        }
-    }
-
-    override fun setUpMainActvityViewModel(mainActivityViewModel: MainActivityContentViewModel) {
         with(binding) {
-            with(mainActivityViewModel) {
-                mainButton.text = buttonTitle
-                mainTitle.text = title
+            mainButton.text = resources.getText(com.mstudio.superheroarch.R.string.main_button_title)
+            mainTitle.text = resources.getText(com.mstudio.superheroarch.R.string.main_title_not_pressed)
+            mainButton.setOnClickListener {
+                binding.mainTitle.text = resources.getText(R.string.main_title_pressed)
             }
         }
-    }
-
-    override fun updateTitle(newTitle: String) {
-        binding.mainTitle.text = newTitle
     }
 }
