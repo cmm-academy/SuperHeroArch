ebba6cfe2eaf2a703293388ecf5d4be22dd00864
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-07-24 16:56:58 +0200

    step 7 implementation

--- app/src/main/java/com/mstudio/superheroarch/presentation/overview/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/presentation/overview/MainActivity.kt
@@ -7,92 +7,80 @@ import androidx.appcompat.app.AppCompatActivity
 import com.mstudio.superheroarch.R
 import com.mstudio.superheroarch.databinding.MainActivityBinding
 import com.mstudio.superheroarch.presentation.detail.CharacterDetailActivity
-import com.mstudio.superheroarch.remotedatasource.api.RetrofitInstance
-import com.mstudio.superheroarch.remotedatasource.api.RickAndMortyApi
 import com.mstudio.superheroarch.remotedatasource.model.CharactersRemoteEntity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainViewTranslator {
 
     companion object {
         const val CHARACTER_DATA_KEY = "character"
     }
 
     private lateinit var binding: MainActivityBinding
+    private val viewModel: MainViewModel by lazy { MainViewModel(this) }
     private val adapter = CharactersAdapter {
-        goToDetail(it)
+        viewModel.onCharacterClicked(it)
     }
-    private var allCharacters = listOf<CharactersRemoteEntity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel.onCreate()
+        setUpView()
+        setUpListeners()
+    }
+
+    private fun setUpView() {
         with(binding) {
             charactersRv.adapter = adapter
-            getCharacters()
             aliveFilterButton.text = resources.getString(R.string.alive_filter_button_title)
             deadFilterButton.text = resources.getString(R.string.dead_filter_button_title)
             unknownFilterButton.text = resources.getString(R.string.unknown_filter_button_title)
             allFilterButton.text = resources.getString(R.string.all_filter_button_title)
+        }
+    }
+
+    private fun setUpListeners() {
+        with(binding) {
             aliveFilterButton.setOnClickListener {
-                setCharacterFilter(allCharacters, StatusFilters.ALIVE)
+                viewModel.onFilterButtonClicked(StatusFilters.ALIVE)
             }
 
             deadFilterButton.setOnClickListener {
-                setCharacterFilter(allCharacters, StatusFilters.DEAD)
+                viewModel.onFilterButtonClicked(StatusFilters.DEAD)
             }
 
             unknownFilterButton.setOnClickListener {
-                setCharacterFilter(allCharacters, StatusFilters.UNKNOWN)
+                viewModel.onFilterButtonClicked(StatusFilters.UNKNOWN)
             }
 
             allFilterButton.setOnClickListener {
-                setCharacterFilter(allCharacters, StatusFilters.ALL)
+                viewModel.onFilterButtonClicked()
             }
         }
     }
 
-    private fun getCharacters() {
-        val apiService = RetrofitInstance.retrofit().create(RickAndMortyApi::class.java)
-        CoroutineScope(Dispatchers.IO).launch {
-            val result = apiService.getCharacters()
-            withContext(Dispatchers.Main) {
-                if (result.isSuccessful) {
-                    result.body()?.characters?.let { characters ->
-                        binding.charactersRv.visibility = View.VISIBLE
-                        binding.errorBody.visibility = View.GONE
-                        allCharacters = characters
-                        adapter.submitList(characters)
-                    } ?: {
-                        binding.charactersRv.visibility = View.GONE
-                        binding.errorBody.visibility = View.VISIBLE
-                        binding.errorBody.text = resources.getString(R.string.main_title_empty_body)
-                    }
-
-                } else {
-                    binding.errorBody.visibility = View.VISIBLE
-                    binding.errorBody.text = resources.getString(R.string.main_title_error_body)
-                }
-            }
-        }
+    override fun showCharacters(characters: List<CharactersRemoteEntity>) {
+        binding.charactersRv.visibility = View.VISIBLE
+        binding.errorBody.visibility = View.GONE
+        adapter.submitList(characters)
     }
 
-    private fun goToDetail(character: CharactersRemoteEntity) {
+    override fun showEmptyCharactersError() {
+        binding.charactersRv.visibility = View.GONE
+        binding.errorBody.visibility = View.VISIBLE
+        binding.errorBody.text = resources.getString(R.string.main_title_empty_body)
+    }
+
+    override fun showGenericError() {
+        binding.errorBody.visibility = View.VISIBLE
+        binding.errorBody.text = resources.getString(R.string.main_title_error_body)
+    }
+
+    override fun goToDetailScreen(characterSelected: CharactersRemoteEntity) {
         val intent = Intent(this, CharacterDetailActivity::class.java).apply {
-            putExtra(CHARACTER_DATA_KEY, character)
+            putExtra(CHARACTER_DATA_KEY, characterSelected)
         }
         startActivity(intent)
-    }
-
-    private fun setCharacterFilter(allCharacters: List<CharactersRemoteEntity>, filter: StatusFilters) {
-        if (filter != StatusFilters.ALL) {
-            adapter.submitList(allCharacters.filter { it.status.equals(filter.status, ignoreCase = true) })
-        } else {
-            adapter.submitList(allCharacters)
-        }
     }
 }
