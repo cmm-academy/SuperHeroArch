872dd90e7c8039e5ec2a55f4d1a33b334379b4a8
Pablo Mariño Cortés <pablomarinoacn@gmail.com>
2024-07-29 12:32:55 +0200

    step 8 implementation

--- app/src/main/java/com/mstudio/superheroarch/presentation/detail/CharacterDetailActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/presentation/detail/CharacterDetailActivity.kt
@@ -1,22 +1,27 @@
 package com.mstudio.superheroarch.presentation.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import coil.load
+import com.google.android.material.snackbar.Snackbar
 import com.mstudio.superheroarch.R
 import com.mstudio.superheroarch.databinding.CharacterDetailActivityBinding
 import com.mstudio.superheroarch.presentation.overview.MainActivity.Companion.CHARACTER_DATA_KEY
 import com.mstudio.superheroarch.remotedatasource.model.CharactersRemoteEntity
+import com.mstudio.superheroarch.remotedatasource.model.EpisodeRemoteEntity
 
-class CharacterDetailActivity : AppCompatActivity() {
+class CharacterDetailActivity : AppCompatActivity(), CharacterDetailViewTranslator {
     private lateinit var binding: CharacterDetailActivityBinding
+    private val viewModel: CharacterDetailViewModel by lazy { CharacterDetailViewModel(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = CharacterDetailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val character = intent.getSerializableExtra(CHARACTER_DATA_KEY) as CharactersRemoteEntity
+        viewModel.onCharacterReceived(character)
 
         with(binding) {
             topBar.navigationIcon = ContextCompat.getDrawable(this@CharacterDetailActivity, R.drawable.ic_arrow_left)
@@ -31,5 +36,20 @@ class CharacterDetailActivity : AppCompatActivity() {
             originDetail.text = resources.getString(R.string.character_origin, character.origin.name)
             locationDetail.text = resources.getString(R.string.character_location, character.location.name)
         }
+    }
+
+    override fun showEpisode(episode: EpisodeRemoteEntity) {
+        with(binding) {
+            episodeNumberDetail.visibility = View.VISIBLE
+            episodeDateDetail.visibility = View.VISIBLE
+            episodeNumberDetail.text = resources.getString(R.string.first_episode_number, episode.episode)
+            episodeDateDetail.text = resources.getString(R.string.first_episode_air_date, episode.airDate)
+        }
+    }
+
+    override fun showEpisodeError() {
+        binding.episodeNumberDetail.visibility = View.GONE
+        binding.episodeDateDetail.visibility = View.GONE
+        Snackbar.make(findViewById(android.R.id.content), resources.getString(R.string.episode_error_message), Snackbar.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/mstudio/superheroarch/remotedatasource/api/RickAndMortyApi.kt
+++ app/src/main/java/com/mstudio/superheroarch/remotedatasource/api/RickAndMortyApi.kt
@@ -1,8 +1,10 @@
 package com.mstudio.superheroarch.remotedatasource.api
 
+import com.mstudio.superheroarch.remotedatasource.model.EpisodeRemoteEntity
 import com.mstudio.superheroarch.remotedatasource.model.RickAndMortyRemoteEntity
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RickAndMortyApi {
 
@@ -12,4 +14,7 @@ interface RickAndMortyApi {
 
     @GET(GET_CHARACTERS)
     suspend fun getCharacters(): Response<RickAndMortyRemoteEntity>
+
+    @GET("episode/{id}")
+    suspend fun getSingleEpisode(@Path("id") id: Int): Response<EpisodeRemoteEntity>
 }
