7250972c830bb651062a38cd0672cb46275f9959
tallpavel <pavel.larsen@accenture.com>
2025-12-18 12:31:37 +0100

    implemented recyclerview and hide button

--- app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
+++ app/src/main/java/com/mstudio/superheroarch/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mstudio.superheroarch
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -8,9 +9,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView : RecyclerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +27,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = CharacterAdapter(emptyList())
+
     }
 
     private fun setClickListener() {
         val myButton = findViewById<Button>(R.id.button)
         myButton.setOnClickListener {
+            it.visibility = View.GONE
             getCharacters()
         }
     }
 
     private fun getCharacters() {
-        val buttonText = findViewById<TextView>(R.id.buttonText)
-        buttonText.text = getString(R.string.loading_characters)
+
         lifecycleScope.launch {
             try {
                 val response = RetroFitClient.apiService.getCharacters()
-                val firstCharacter = response.results.firstOrNull()
-                buttonText.text = getString(R.string.character_TheFirstCharacter)+" ${firstCharacter?.name} ${firstCharacter?.status} ${firstCharacter?.image}"
+                val adaptor = CharacterAdapter(response.results)
+                println(adaptor)
+                adaptor.notifyItemChanged(response.results.size-1)
+                recyclerView.adapter = adaptor
+
+
+
             } catch (e: Exception) {
-                buttonText.text = getString(R.string.character_load_error, e.message)
+                val errorMessage = "Error: ${e.message}"
+                println(errorMessage)
             }
         }
     }
